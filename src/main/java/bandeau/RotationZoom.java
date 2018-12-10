83643ed62db3c4d62caa6d44c6a82ff6bc6212e7
mohamet003 <mohamet.kone@etud.univ-jfc.fr>
2018-12-10 18:02:49 +0100

    first commit

---
+++ src/main/java/bandeau/RotationZoom.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bandeau;
+
+import java.awt.Font;
+
+/**
+ *
+ * @author mkone03
+ */
+public class RotationZoom extends TypeEffect{
+    
+    public RotationZoom() {
+
+    }
+    
+    
+    
+}
---
+++ src/main/java/bandeau/Senario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bandeau;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author mkone03
+ */
+public class Senario {
+    List<TypeEffect> ListeTypeDeffect = new  LinkedList<>();
+    
+    
+    
+}
